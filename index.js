0648600458d587a789f686af45c21a1d252e7662
Syha Ban <syhaban@hotmail.com>
2023-04-16 20:22:25 -0400

    Adds index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (num1, num2) => {
+    console.log (`Dividing ${num1}`);
+    console.log (`Dividing ${num2}`);
+    return num1 / num2;
+}
+
+const square = (num3) => {
+    console.log (`Multiplying ${num3}`)
+    return num3 * num3;
+}
+
+const add = (num4, num5) => {
+    console.log (`Adding ${num4}`)
+    console.log (`Adding ${num5}`)
+    return num4 + num5;
+}
